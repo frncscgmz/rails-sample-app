8c0f04e887c2488e7af880fcde5ee046a4661777
Francisco Gomez <mail@frncscgmz.me>
2013-09-18 15:33:24 -0700

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,23 @@
 SampleApp::Application.routes.draw do
-  #get "users/new"
-  resources :users
-  resources :sessions, only: [:new, :create, :destroy]
-  resources :microposts, only: [:create, :destroy]
+   resources :users do
+      member do
+         get :following, :followers
+      end
+   end
+   resources :sessions,      only: [:new, :create, :destroy]
+   resources :microposts,    only: [:create, :destroy]
+   resources :relationships, only: [:create, :destroy]
 
-  root  'static_pages#home'
-  match '/signup',  to: 'users#new',            via: 'get'
-  match '/signin',  to: 'sessions#new',         via: 'get'
-  match '/signout', to: 'sessions#destroy',     via: 'delete'
-  match '/help',    to: 'static_pages#help',    via: 'get'
-  match '/about',   to: 'static_pages#about',   via: 'get'
-  match '/contact', to: 'static_pages#contact', via: 'get'
+   root  'static_pages#home'
+   match '/signup',  to: 'users#new',            via: 'get'
+   match '/signin',  to: 'sessions#new',         via: 'get'
+   match '/signout', to: 'sessions#destroy',     via: 'delete'
+   match '/help',    to: 'static_pages#help',    via: 'get'
+   match '/about',   to: 'static_pages#about',   via: 'get'
+   match '/contact', to: 'static_pages#contact', via: 'get'
 
-  #get "static_pages/home"
-  #get "static_pages/help"
-  #get "static_pages/about"
-  #get "static_pages/contact"
+   #get "static_pages/home"
+   #get "static_pages/help"
+   #get "static_pages/about"
+   #get "static_pages/contact"
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe User do
    before do 
       @user = User.new(name: "Example User", email: "user@example.com",
-                      password: "foobar", password_confirmation: "foobar")
+                       password: "foobar", password_confirmation: "foobar")
    end
 
    subject { @user }
@@ -18,6 +18,13 @@ describe User do
    it { should respond_to(:admin) }
    it { should respond_to(:microposts) }
    it { should respond_to(:feed) }
+   it { should respond_to(:relationships) }
+   it { should respond_to(:followed_users) }
+   it { should respond_to(:reverse_relationships) }
+   it { should respond_to(:followers) }
+   it { should respond_to(:following?) }
+   it { should respond_to(:follow!) }
+   it { should respond_to(:unfollow!) }
 
    it { should be_valid }
    it { should_not be_admin }
@@ -47,25 +54,25 @@ describe User do
    end
 
    describe "when email format is invalid" do
-    it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+      it "should be invalid" do
+         addresses = %w[user@foo,com user_at_foo.org example.user@foo.
                      foo@bar_baz.com foo@bar+baz.com]
-      addresses.each do |invalid_address|
-        @user.email = invalid_address
-        expect(@user).not_to be_valid
+                     addresses.each do |invalid_address|
+                        @user.email = invalid_address
+                        expect(@user).not_to be_valid
+                     end
       end
-    end
-  end
+   end
 
    describe "when email format is valid" do
-    it "should be valid" do
-      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-      addresses.each do |valid_address|
-        @user.email = valid_address
-        expect(@user).to be_valid
+      it "should be valid" do
+         addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+         addresses.each do |valid_address|
+            @user.email = valid_address
+            expect(@user).to be_valid
+         end
       end
-    end
-  end
+   end
 
    describe "when email address is already taken" do
       before do
@@ -79,7 +86,7 @@ describe User do
    describe "when password is not present" do
       before do
          @user = User.new(name: "Example User", email: "user@example.com",
-                         password: " ", password_confirmation: " ")
+                          password: " ", password_confirmation: " ")
       end
       it { should_not be_valid }
    end
@@ -111,7 +118,6 @@ describe User do
    end
 
    describe "microposts associations" do
-
       before { @user.save }
       let!(:older_micropost) do
          FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
@@ -137,15 +143,44 @@ describe User do
          let(:unfollowed_post) do
             FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
          end
+         let(:followed_user) { FactoryGirl.create(:user) }
+
+         before do
+            @user.follow!(followed_user)
+            3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+         end
 
          its(:feed) { should include(newer_micropost) }
          its(:feed) { should include(older_micropost) }
          its(:feed) { should_not include(unfollowed_post) }
+         its(:feed) do
+            followed_user.microposts.each do |micropost|
+               should include(micropost)
+            end
+         end
       end
    end
 
-   #describe "remember token" do
-   #   before { @user.save }
-   #   its(:rememeber_token) { should_not be_blank  }
-   #end
+   describe "following" do
+      let(:other_user) { FactoryGirl.create(:user) }
+      before do
+         @user.save
+         @user.follow!(other_user)
+      end
+
+      it { should be_following(other_user) }
+      its(:followed_users) { should include(other_user) }
+
+      describe "followed user" do
+         subject { other_user }
+         its(:followers) { should include(@user) }
+      end
+
+      describe "and unfollowing" do
+         before { @user.unfollow!(other_user) }
+
+         it { should_not be_following(other_user) }
+         its(:followed_users) { should_not include(other_user) }
+      end
+   end
 end
